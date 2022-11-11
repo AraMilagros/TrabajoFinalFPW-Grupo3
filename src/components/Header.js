9fca0dc93e018e95ceaa317623fe16a4334a621a
AraMilagros <araapalenque@gmail.com>
2022-11-11 08:13:11 -0300

    Se agrego estilos al menu y se lo separo como un componente Header. Se creo una carpeta components y pages: componentes contiene Header y pages las paginas de ErrorPage y Home. Tambien se cambio de ubitacion la carpeta json hacia la carpeta de assets. Ara Palenque

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Config from '../phaser/Config';
+import ErrorPage from '../pages/ErrorPage';
+
+import '../assets/css/Header.css';
+export default function Header() {
+    return (
+        <div className="header-contenedor">
+            <nav>
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="phaser">Juego Phaser</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="react">Juego React</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Desarrolladores</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="phaser" element={<Config />} />
+                        <Route path="react" element={<ErrorPage />} /> {/* Aqui iria el juego hecho solo con react - solo se puso ErrorPage para ejemplo */}
+                        <Route path="*" element={<ErrorPage />} />
+                    </Routes>
+                </div>
+            </div>
+
+        </div>
+    );
+}
