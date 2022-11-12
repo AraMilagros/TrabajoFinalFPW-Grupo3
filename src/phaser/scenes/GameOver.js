30d100bc1847874253220a0b997297a44842f672
LucasGGz <112144033+LucasGGz@users.noreply.github.com>
2022-11-11 21:59:43 -0300

    Se agrego boton para reintentar al perder -Lucas Martinez

--- src/phaser/scenes/GameOver.js
+++ src/phaser/scenes/GameOver.js
@@ -1,17 +1,21 @@
 import { ButtonLost } from "../botons/ButtonLost";
+import { ButtonRestart } from "../botons/ButtonRestart";
 export default class Menu extends Phaser.Scene{
     constructor(){
         super({key : 'GameOver'});
         this.play = new ButtonLost(this);
+        this.restart= new ButtonRestart(this);
     }
 
     preload(){
         this.play.preload();
+        this.restart.preload(); //se llama al preload del restart
     }
  
     create(){
         this.game.config.backgroundColor.setTo(0, 0, 0);
         this.add.image(500,200,'G.O').setScale(1); //se agrega a la imagen y se modifica el tamaño
         this.play.create();//Aqui se llama la funcion para mostrar el buttonLost en la escena
+        this.restart.create();//Aqui se llama la funcion para mostrar el buttonRestart en la escena
     }
 }
