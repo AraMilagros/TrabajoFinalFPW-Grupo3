48e934ef50fa0dc2a429a25f4e70c5e8c0c91d17
AraMilagros <araapalenque@gmail.com>
2022-11-12 08:06:33 -0300

    Se agrego llamado de escena de victoria. Ara Palenque

--- src/phaser/gameObjects/Player.js
+++ src/phaser/gameObjects/Player.js
@@ -49,7 +49,7 @@ export default class Player extends Phaser.GameObjects.Sprite{
             this.player.anims.play('turn');
         }
         if (this.cursors.up.isDown && this.player.body.onFloor()) { //con "body.onFloor()" controla que no salte 2 veces
-            this.player.setVelocityY(-470);
+            this.player.setVelocityY(-400);
            // this.checkJump();//Es evitar que pueda saltar en el aire
         }
     }
