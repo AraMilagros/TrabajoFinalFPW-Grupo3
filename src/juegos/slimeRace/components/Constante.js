9bfaecd70c1a27c233c6abc39e05d2199b43376a
Areyss <arielryss@gmail.com>
2022-11-13 19:13:00 -0300

    Se agrego comentarios en el juego React

--- src/juegos/slimeRace/components/Constante.js
+++ src/juegos/slimeRace/components/Constante.js
@@ -1,10 +1,12 @@
-export const PISTA = ["box-llena" ,"box-vacia", "box-vacia", 
+//variables constates definidas como arrays que se exportan para ser usadas en el juego react
+//estas variables representas las pistas de carrera
+export const PISTA = ["box-llena" ,"box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-meta"];
 
-export const PISTA2 = ["box-llena" ,"box-vacia", "box-vacia", 
+export const PISTA2 = ["box-llena" ,"box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
                 "box-vacia", "box-vacia", "box-vacia",
--- src/juegos/slimeRace/components/PistaCarrera.js
+++ src/juegos/slimeRace/components/PistaCarrera.js
@@ -1,8 +1,10 @@
-
 import '../assets/css/pistaCarrera.css';
 
+//componente que "dibuja la pista"
 export default function PistaCarrera({pista}){
+    //funcionque recorre y retorna el array pista
     const crearPista=()=>{
+        //tipo: tipo de dato string que decide como debe llamarse la clase del div a dibujar
         return pista.map((tipo,index)=>
         <div className={tipo}
             key={index}>
