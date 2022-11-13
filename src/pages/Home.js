cd5a4ea117c2827576245b937be529e650898b5b
AraMilagros <araapalenque@gmail.com>
2022-11-13 16:48:21 -0300

    Se agrego contenido en el home. AraPalenque

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import '../assets/css/Home.css';
+import phaser from '../assets/img/phaser3.png';
+import reactjs from '../assets/img/reactjs.png';
 
 export default function Home() {
     return (
-        <div>
-            <h2 className='titulo-home'>Home Page</h2>
-        </div>
+        <>
+            <div className="contenedor-home">
+                <div className="titulo-home">
+                    <h3>FUNDAMENTOS DE PROGRAMACIÓN WEB</h3>
+                    <h3>TRABAJO FINAL</h3>
+                    <h3>GRUPO 3</h3>
+                </div>
+
+                <div className="juegos">
+                    <div className="titulo-phaser">
+                        <img src={phaser} className="img-home"></img>
+                        <div className="descripcion">
+                            <h4>Juego con Phaser 3</h4>
+                            <h5>JUMPECT</h5>
+                            <p>
+                                El jugador debera saltar hasta la meta acumulando puntajes antes de que el tiempo se termine
+                            </p>
+                        </div>
+                        <a href="juegos/Jumpect" className="btn btn-dark">Jugar ahora</a>
+                    </div>
+
+                    <div className="titulo-reacthome">
+                        <img src={reactjs} className="img-home"></img>
+                        <div className="descripcion">
+                            <h4>Juego con ReactJS</h4>
+                            <h5>SLIME RACE</h5>
+                            <p>
+                                Juego de dos jugadores, donde cada uno responderá preguntas para poder alcanzar la meta
+                            </p>
+                        </div>
+                        <a href="juegos/SlimeRace" className="btn btn-dark">Jugar ahora</a>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
